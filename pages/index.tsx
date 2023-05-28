7734848959b6f7c127d72b2178991ccabd6205a5
Guilherme Caramori <gcaramori16@gmail.com>
2023-05-28 20:36:03 -0300

    Added Page transition to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/header/navbar'
+import PageTransition from '@/components/shared/pageTransition'
+
+type IndexPageRef = React.ForwardedRef<HTMLDivElement>
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home(ref: IndexPageRef) {
   return (
-    <>
+    <PageTransition ref={ref}>
       <Navbar />
 
       <main className={`flex h-full flex-col items-center justify-between p-24 ${inter.className} overflow-hidden`}>
         
       </main>
-    </>
+    </PageTransition>
   )
 }
